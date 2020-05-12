59967ae2944f1a7bcb0588505dc8fb2dda8dfb80
gabe3 <Dinya7991>
2020-05-12 09:04:44 -0400

    Added $

--- src/components/AccountItem.js
+++ src/components/AccountItem.js
@@ -7,7 +7,7 @@ const AccountItem = props => {
     return (
         <li className="list-group-item" id = {props.account._id}>
             <h4>{ props.account.name } </h4>
-            Balance: { props.account.balance }
+            Balance: ${ props.account.balance }
             <button type="button"
                     onClick={() => props.removeAccount(props.account)}
                     className="btn btn-primary" style={{ float: 'right' }}>
